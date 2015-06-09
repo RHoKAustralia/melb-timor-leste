7d2009c523ec3770e5aa413e6f0f428d03169c25
Lachlan Barclay <clockwise.music@gmail.com>
2015-06-10 09:11:44 +1000

    added "select gender" screen

--- LanguageRecorder/app/src/main/java/com/example/lachlan/myfirstapp/interview/InterviewAgeActivity.java
+++ LanguageRecorder/app/src/main/java/com/example/lachlan/myfirstapp/interview/InterviewAgeActivity.java
@@ -27,23 +27,16 @@ public class InterviewAgeActivity extends ActionBarActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_interview_age, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
@@ -55,13 +48,13 @@ public class InterviewAgeActivity extends ActionBarActivity {
         if (age.trim().length() == 0) {
             Toast toast = Toast.makeText(getApplicationContext(), "Please enter an age", Toast.LENGTH_SHORT);
             toast.show();
-        } else {
-
+        }
+        else {
             _person.age = Integer.parseInt(age);
 
-//            Intent intent = new Intent(this, InterviewAgeActivity.class);
- //           intent.putExtra("person", p);
-  //          startActivity(intent);
+            Intent intent = new Intent(this, InterviewGenderActivity.class);
+            intent.putExtra("person", _person);
+            startActivity(intent);
         }
 
     }
---
+++ LanguageRecorder/app/src/main/java/com/example/lachlan/myfirstapp/interview/InterviewGenderActivity.java
@@ -0,0 +1,95 @@
+package com.example.lachlan.myfirstapp.interview;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.lachlan.myfirstapp.R;
+import com.example.lachlan.myfirstapp.code.Person;
+
+import java.util.ArrayList;
+
+public class InterviewGenderActivity extends ActionBarActivity {
+
+    private Person _person;
+    private String selectedGender;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interview_gender);
+
+        Intent intent = getIntent();
+        _person = (Person) intent.getSerializableExtra("person");
+
+        populateGenders();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_interview_gender, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void populateGenders() {
+        ListView lvStudies = (ListView) findViewById(R.id.genderListView);
+        ArrayList<String> genders = new ArrayList<String>();
+        genders.add("Mane");
+        genders.add("Feto");
+
+        ArrayAdapter<String> aaStudies = new ArrayAdapter<String>(
+                this, android.R.layout.simple_list_item_1, genders);
+
+        aaStudies.setDropDownViewResource(android.R.layout.simple_expandable_list_item_1);
+
+        lvStudies.setAdapter(aaStudies);
+        lvStudies.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selectedGender = (String) parent.getItemAtPosition(position);
+            }
+        });
+    }
+
+    public void continueButtonClick(android.view.View view) {
+
+        if (selectedGender == null) {
+            Toast toast = Toast.makeText(getApplicationContext(), "Please select a gender", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+        else {
+            _person.gender = selectedGender;
+
+            Intent intent = new Intent(this, InterviewGenderActivity.class);
+            intent.putExtra("person", _person);
+            startActivity(intent);
+        }
+
+    }
+
+}
